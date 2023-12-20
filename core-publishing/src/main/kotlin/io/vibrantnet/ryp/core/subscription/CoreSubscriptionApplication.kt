4cff9f37f1eecbd45e63391f88df467ff1a57fef
Nils Codes <contact@hazelnet.io>
2023-12-20 15:31:30 -0800

    RYP-47 Adding publishing core service Spring Boot application
    Also consolidated imports across projects.

---
+++ core-publishing/src/main/kotlin/io/vibrantnet/ryp/core/subscription/CoreSubscriptionApplication.kt
@@ -0,0 +1,11 @@
+package io.vibrantnet.ryp.core.subscription
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoreSubscriptionApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoreSubscriptionApplication>(*args)
+}
---
+++ core-publishing/src/test/kotlin/io/vibrantnet/ryp/core/subscription/CoreSubscriptionApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.vibrantnet.ryp.core.subscription
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoreSubscriptionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
