f34d9d4d52709f436e519773d28aa361f7f1e8dd
Nils Codes <contact@hazelnet.io>
2023-12-20 16:15:36 -0800

    RYP-66 Added publishing as last initial core service

---
+++ core-publishing/src/test/kotlin/io/vibrantnet/ryp/core/publishing/CorePublishingApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.vibrantnet.ryp.core.publishing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CorePublishingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "reach-your-people"
 include("core-verification")
 include("core-subscription")
+include("core-publishing")
