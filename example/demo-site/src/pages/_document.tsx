90eb3b7a649a9d0e516f101fd0bac3891bb3df70
Nils Peuser <nils@peusers.org>
2024-07-02 18:31:01 +0000

    RYP-140 Added SEO improvements and corrected names and titles for Twitter cards
    * RYP-140 Added SEO improvements and corrected names and titles for Twitter cards

--- example/demo-site/src/pages/_document.tsx
+++ example/demo-site/src/pages/_document.tsx
@@ -1,16 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+const description = "RYP is a communication and announcement hub for Cardano and other blockchains. It allows users to connect their social media and wallet information and receive notifications from verified projects through the channels they prefer, while ensuring security, privacy and providing a great user experience.";
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content={description} />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@VibrantNet_io" />
-        <meta name="twitter:title" content="vibrantnet.io Community Integration System" />
-        <meta name="twitter:description" content="Vibrant is a Community Integration Tool for the blockchain Cardano. It comprises a Discord bot and other services to allow you to connect your community by means of blockchain technology." />
-        <meta name="twitter:image" content="/logo512.png" />
-        <link rel="apple-touch-icon" href="/logo192.png" />
+        <meta name="twitter:title" content="Reach Your People (RYP)" />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={`${process.env.NEXT_PUBLIC_API_URL}/logo512.png`} />
+        <link rel="apple-touch-icon" href={`${process.env.NEXT_PUBLIC_API_URL}/logo192.png`} />
       </Head>
       <body>
         <Main />
