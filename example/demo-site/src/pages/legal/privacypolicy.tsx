5851bdd29e70c0f1d3c88694fecf3a86ff5b81db
Nils Peuser <nils@peusers.org>
2024-08-21 23:47:27 +0000

    RYP-169 Added Termly policies to website as JS snippets that embed an IFrame
    * RYP-169 Added Termly policies to website as JS snippets that embed an IFrame

--- example/demo-site/src/pages/legal/privacypolicy.tsx
+++ example/demo-site/src/pages/legal/privacypolicy.tsx
@@ -1,16 +1,11 @@
-import { GetServerSideProps } from 'next';
+import Head from "next/head";
+import TermlyPrivacyPolicy from "@/components/legal/TermlyPrivacyPolicy";
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    redirect: {
-      destination: 'https://app.termly.io/document/privacy-policy/03f7e652-321e-4bc6-a043-a7880d90b223',
-      permanent: false,
-    },
-  };
-};
-
-const PrivacyPolicy = () => {
-  return null; // This component will not be rendered
-};
-
-export default PrivacyPolicy;
+export default function Home() {
+  return (<>
+    <Head>
+      <title>RYP: Privacy Policy</title>
+    </Head>
+    <TermlyPrivacyPolicy />
+  </>);
+}
