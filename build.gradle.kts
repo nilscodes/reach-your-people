adcaac5f7f2a8987274bfd7cacbe387c08636135
Nils Codes <contact@hazelnet.io>
2024-08-22 17:55:29 -0700

    RYP-179 Bumping Kotlin to 2.0.20
    CVEs were still active, and other libraries had already been bumped.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,10 @@ extra["equalsVerifierVersion"] = "3.16.1"
 extra["slf4jVersion"] = "2.0.16"
 
 plugins {
-    kotlin("jvm") version "1.9.23" apply false
-    kotlin("plugin.spring") version "1.9.23" apply false
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.23" apply false
-    id("org.jetbrains.kotlin.plugin.noarg") version "1.9.23" apply false
+    kotlin("jvm") version "2.0.20" apply false
+    kotlin("plugin.spring") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.allopen") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.noarg") version "2.0.20" apply false
     id("org.sonarqube") version "5.1.0.4882"
     id("org.springframework.boot") version "3.3.2" apply false
     id("io.spring.dependency-management") version "1.1.5" apply false
